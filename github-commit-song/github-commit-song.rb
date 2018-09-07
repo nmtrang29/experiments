54fef6ed7454f6e94f449f7eff903b9706531127
Trang Nguyen <nguyenminhtrang29@gmail.com>
2018-09-07 23:52:10 +0200

    github commit song

---
+++ github-commit-song/github-commit-song.rb
@@ -0,0 +1,368 @@
+sleep 1.0
+play 100, amp: 0.1
+sleep 2.0
+
+#oct4
+play 50, amp: 0.1
+sleep 5.75
+
+#oct27
+play 55, amp: 0.2
+sleep 1.5
+
+#nov2
+play 55, amp: 0.2
+sleep 0.5
+
+#nov4
+play 50, amp: 0.1
+sleep 4.5
+
+#nov22
+play 57, amp: 0.3
+sleep 1.25
+
+#nov27
+play 62, amp: 0.4
+sleep 0.5
+
+#nov29-30
+play 55, amp: 0.2
+sleep 0.25
+play 62, amp: 0.4
+sleep 0.5
+
+#dec1
+play 50, amp: 0.1
+sleep 0.75
+
+#dec3-4
+play 50, amp: 0.1
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.5
+
+#dec7-8
+play 62, amp: 0.4
+sleep 0.25
+play 76, amp: 0.8
+sleep 1.0
+
+#dec7-8
+play 55, amp: 0.2
+sleep 0.25
+play 62, amp: 0.4
+sleep 1.25
+
+#dec18
+play 50, amp: 0.1
+sleep 4.25
+
+#jan4
+play 50, amp: 0.1
+sleep 1.5
+
+#jan11-12
+play 55, amp: 0.2
+sleep 0.25
+play 57, amp: 0.3
+sleep 0.75
+
+#jan15-17
+play 78, amp: 0.8
+sleep 0.25
+play 57, amp: 0.3
+sleep 0.25
+play 55, amp: 0.2
+sleep 0.5
+
+#jan19
+play 55, amp: 0.2
+sleep 0.75
+
+#jan22-24
+play 55, amp: 0.2
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.25
+play 57, amp: 0.3
+sleep 0.5
+
+#jan26
+play 55, amp: 0.2
+sleep 0.75
+
+#jan29-30
+play 50, amp: 0.1
+sleep 0.25
+play 50, amp: 0.1
+sleep 1.5
+
+#feb5-7
+play 55, amp: 0.2
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.25
+play 64, amp: 0.5
+sleep 1.25
+
+#feb12
+play 50, amp: 0.1
+sleep 0.25
+
+#feb14
+play 55, amp: 0.2
+sleep 2.0
+
+#feb22-23
+play 50, amp: 0.1
+sleep 0.25
+play 69, amp: 0.6
+sleep 0.75
+
+#feb26-27
+play 55, amp: 0.2
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.5
+
+#mar1-2
+play 50, amp: 0.1
+sleep 0.25
+play 50, amp: 0.1
+sleep 2.5
+
+#mar12-13
+play 57, amp: 0.3
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.75
+
+#mar16
+play 55, amp: 0.2
+sleep 1.0
+
+#mar20
+play 57, amp: 0.3
+sleep 5.0
+
+#apr9
+play 55, amp: 0.2
+sleep 0.75
+
+#apr12-13
+play 50, amp: 0.1
+sleep 0.25
+play 55, amp: 0.2
+sleep 0.75
+
+#apr16-19
+play 50, amp: 0.1
+sleep 0.25
+play 55, amp: 0.2
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.25
+play 55, amp: 0.2
+sleep 1.0
+
+#apr23
+play 50, amp: 0.1
+sleep 1.0
+
+#apr27
+play 55, amp: 0.2
+sleep 1.25
+
+#may2-3
+play 55, amp: 0.2
+sleep 0.25
+play 57, amp: 0.3
+sleep 1.0
+
+#may7-9
+play 57, amp: 0.3
+sleep 0.25
+play 62, amp: 0.4
+sleep 0.25
+play 57, amp: 0.3
+sleep 1.25
+
+#may14
+play 55, amp: 0.2
+sleep 0.5
+
+#may16-18
+play 55, amp: 0.2
+sleep 0.25
+play 69, amp: 0.6
+sleep 0.25
+play 62, amp: 0.4
+sleep 1.0
+
+#may22
+play 55, amp: 0.2
+sleep 0.25
+play 50, amp: 0.1
+sleep 1.5
+
+#may29-31
+play 57, amp: 0.3
+sleep 0.25
+play 62, amp: 0.4
+sleep 0.25
+play 55, amp: 0.2
+sleep 1.0
+
+#jun4-8
+play 55, amp: 0.2
+sleep 0.25
+play 57, amp: 0.3
+sleep 0.25
+play 57, amp: 0.3
+sleep 0.25
+play 55, amp: 0.2
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.75
+
+#jun11-15
+play 50, amp: 0.1
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.25
+play 64, amp: 0.5
+sleep 0.25
+play 76, amp: 0.8
+sleep 0.25
+play 57, amp: 0.3
+sleep 0.75
+
+#jun18-21
+play 55, amp: 0.2
+sleep 0.25
+play 62, amp: 0.4
+sleep 0.25
+play 64, amp: 0.5
+sleep 0.25
+play 57, amp: 0.3
+sleep 1.5
+
+#jun27-29
+play 57, amp: 0.3
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.25
+play 57, amp: 0.3
+sleep 1.0
+
+#jul2
+play 71, amp: 0.7
+sleep 0.5
+
+#jul4-6
+play 57, amp: 0.3
+sleep 0.25
+play 55, amp: 0.2
+sleep 0.25
+play 62, amp: 0.4
+sleep 0.75
+
+#jul9
+play 50, amp: 0.1
+sleep 0.75
+
+#jul12-13
+play 64, amp: 0.5
+sleep 0.25
+play 57, amp: 0.3
+sleep 0.75
+
+#jul16
+play 78, amp: 0.9
+sleep 0.5
+
+#jul18-19
+play 64, amp: 0.5
+sleep 0.25
+play 62, amp: 0.4
+sleep 0.75
+
+#jul22-26
+play 50, amp: 0.1
+sleep 0.25
+play 90, amp: 0.8
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.25
+play 57, amp: 0.3
+sleep 1.0
+
+#jul30-31-aug1-2
+play 55, amp: 0.2
+sleep 0.25
+play 69, amp: 0.6
+sleep 0.25
+play 69, amp: 0.6
+sleep 0.25
+play 64, amp: 0.5
+sleep 1.25
+
+#aug7-14
+play 55, amp: 0.2
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.25
+play 71, amp: 0.7
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.25
+play 62, amp: 0.4
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.25
+play 64, amp: 0.5
+sleep 0.5
+
+#aug16-17
+play 55, amp: 0.2
+sleep 0.25
+play 62, amp: 0.4
+sleep 0.5
+
+#aug19-20
+play 50, amp: 0.1
+sleep 0.25
+play 69, amp: 0.6
+sleep 0.5
+
+#aug22-30
+play 57, amp: 0.3
+sleep 0.25
+play 64, amp: 0.5
+sleep 0.25
+play 57, amp: 0.3
+sleep 0.25
+play 69, amp: 0.6
+sleep 0.25
+play 57, amp: 0.3
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.25
+play 50, amp: 0.1
+sleep 0.5
+
+#sep 2-4
+play 69, amp: 0.6
+sleep 0.25
+play 62, amp: 0.4
+sleep 0.25
+play 76, amp: 0.8
+sleep 0.0
+
